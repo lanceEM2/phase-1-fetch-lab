92a0463607e59bdbbdef82efd92599aa735279de
Dylan <dylanlancekemboi@gmail.com>
2023-10-17 13:40:29 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')  // Make a fetch request to the API
+    .then(response => response.json())  // Convert the response to JSON
+    .then(data => renderBooks(data));   // Pass the JSON data to renderBooks
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.textContent = book.name;
     main.appendChild(h2);
   });
 }
